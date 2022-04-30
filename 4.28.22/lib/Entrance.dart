16355bfbaa00a02b9c121b84cba0271e17538e37
SONTAEGUK <101920873+SONTAEGUK@users.noreply.github.com>
2022-04-30 17:14:07 +0900

    Update Entrance.dart

--- 4.28.22/lib/Entrance.dart
+++ 4.28.22/lib/Entrance.dart
@@ -1,13 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'Lobby.dart';
-
 void main() {
-  runApp(EntrancePage());
+  runApp(MyApp());
 }
 
-class EntrancePage extends StatelessWidget {
-  const EntrancePage({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,139 +15,158 @@ class EntrancePage extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.black,
-          body: Center(
-            child: Column(
-              children: [
-                Container(
-                  height: 30.0,
-                  width: 500.0,
-                  color: Colors.black,
-                ),
-                Text(
-                  '북클럽 입장',
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                ),
-                Container(
-                  height: 40.0,
-                  width: 500.0,
-                  color: Colors.black,
-                ),
-                Container(
-                  child: Image.asset('lib/images/Splash_Character.png'),
-                  height: 180.0,
-                  width: 400.0,
-                ),
-                Container(
-                  height: 20.0,
-                  width: 500.0,
-                  color: Colors.black,
-                ),
-                Text(
-                  'bookiebookie33',
-                  style: TextStyle(fontSize: 20, color: Colors.blue),
-                ),
-                Container(
-                  height: 50.0,
-                  width: 500.0,
-                  color: Colors.black,
-                ),
-                ElevatedButton.icon(
-                  onPressed: () {
-                    // LobbyPage로 이동
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => LobbyPage()),
-                    );
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.red,
-                    minimumSize: Size(380, 50),
+          body: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    height: 20.0,
+                    width: 500.0,
+                    color: Colors.black,
                   ),
-                  label: Text(
-                    '내 북클럽 입장하기',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
+                  Text(
+                    '북클럽 입장',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 500.0,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    child: Image.asset('lib/images/egg.png'),
+                    height: 150.0,
+                    width: 300.0,
+                  ),
+                  Container(
+                    height: 20.0,
+                    width: 500.0,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    width: 200,
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.red),
+                        ),
+                        suffixIcon: Icon(
+                          CupertinoIcons.pen,
+                          color: Colors.white,
+                        ),
+                        hintText: '닉네임을 입력해주세요',
+                        hintStyle: TextStyle(color: Colors.grey[600]),
+                      ),
                     ),
                   ),
-                  icon: Icon(Icons.add, size: 18, color: Colors.white),
-                ),
-                Container(
-                  height: 60.0,
-                  width: 500.0,
-                  color: Colors.black,
-                ),
-                Text(
-                  '''다른 사람의 초대번호를 입력하고
-    북클럽 방으로 입장해보세요.''',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15,
+                  Container(
+                    height: 50.0,
+                    width: 500.0,
+                    color: Colors.black,
                   ),
-                ),
-                Container(
-                  height: 10.0,
-                  width: 500.0,
-                  color: Colors.black,
-                ),
-                Row(
-                  children: [
-                    Container(
-                      width: 200.0,
-                      child: Expanded(
-                        child: TextField(
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      // LobbyPage로 이동
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => LobbyPage()),
+                      );
+                    },
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      minimumSize: Size(360, 50),
+                    ),
+                    label: Text(
+                      '내 북클럽 입장하기',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                    icon: Icon(Icons.add, size: 18, color: Colors.white),
+                  ),
+                  Container(
+                    height: 40.0,
+                    width: 500.0,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    '''다른 사람의 초대번호를 입력하고
+    북클럽 방으로 입장해보세요.''',
+                    style: TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 15,
+                    ),
+                  ),
+                  Container(
+                    height: 10.0,
+                    width: 500.0,
+                    color: Colors.black,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 200,
+                        color: Colors.grey[850],
+                        child: TextFormField(
                           textAlign: TextAlign.center,
                           style: TextStyle(color: Colors.white),
                           decoration: InputDecoration(
-                            labelText: "영문,숫자 9자리 조합",
-                            labelStyle: TextStyle(color: Colors.grey),
+                            fillColor: Colors.white,
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                            ),
+                            hintText: '영문,숫자 9자리 조합',
+                            hintStyle: TextStyle(color: Colors.grey),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Container(
-                      height: 20.0,
-                      width: 2.0,
-                      color: Colors.grey,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    ElevatedButton.icon(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.grey,
-                        minimumSize: Size(30, 50),
+                      Container(
+                        height: 50.0,
+                        width: 2.0,
+                        color: Colors.grey,
                       ),
-                      label: Text(
-                        '입장하기',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontSize: 18,
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          // LobbyPage로 이동
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LobbyPage()),
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey[850],
+                          minimumSize: Size(30, 50),
                         ),
+                        label: Text(
+                          '입장하기',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18,
+                          ),
+                        ),
+                        icon: Icon(Icons.add, size: 18, color: Colors.red),
                       ),
-                      icon: Icon(Icons.add, size: 18, color: Colors.white),
-                    ),
-                  ],
-                ),
-                Container(
-                  height: 10.0,
-                  width: 500.0,
-                  color: Colors.black,
-                ),
-                Text(
-                  '초대 번호를 다시 확인해주세요.',
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 10,
+                    ],
                   ),
-                ),
-              ],
+                  Container(
+                    height: 10.0,
+                    width: 500.0,
+                    color: Colors.black,
+                  ),
+                  Text(
+                    '초대 번호를 다시 확인해주세요.',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 10,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
