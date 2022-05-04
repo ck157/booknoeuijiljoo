2789adb619651aed997e93d0413c5609462a6fab
JazzyJiwon <101383114+JazzyJiwon@users.noreply.github.com>
2022-05-05 00:38:26 +0900

    Add files via upload

--- 4.28.22/lib/bookclub_service.dart
+++ 4.28.22/lib/bookclub_service.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+import 'dart:math';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 //구현 기능
@@ -10,10 +14,42 @@ import 'package:flutter/material.dart';
 // delete_comment : 댓글 삭제하기
 
 class ClubService extends ChangeNotifier {
-  final ClubCollection = FirebaseFirestore.instance.collection('clubs');
+  final ClubCollection = FirebaseFirestore.instance.collection('Book');
   // final membersCollection = FirebaseFirestore.instance.collection('clubs').doc();
 
-  void create_club(
+  getClub(String clubId) async {
+    // 단하나의 스냅샷 조회
+    DocumentSnapshot<Map<String, dynamic>> snapshot = await ClubCollection.doc(clubId).get();
+    print(snapshot.data());
+    // 컬렉션의 스냅샷 리스트 조회
+    QuerySnapshot<Map<String, dynamic>> snapshot2 =
+        await ClubCollection.doc(clubId).collection('members').where('uid', isEqualTo: 'uid111').get();
+
+    // 컬렉션에 스냅샷 추가
+    DocumentReference<Map<String, dynamic>> ref = await ClubCollection.add({"uid": "황성현이지롱"});
+
+    inspect(ref);
+    print('ID?????${ref.id}');
+
+    // print('snapshot2 Exist? ${snapshot2.docs.length}');
+    // snapshot2.docs.forEach((element) {
+    //   print(element.data());
+    // });
+
+    return snapshot.data();
+  }
+
+  getClubList() async {
+    QuerySnapshot<Map<String, dynamic>> snapshot = await ClubCollection.get();
+    List<QueryDocumentSnapshot<Map<String, dynamic>>> querySnapShotList = snapshot.docs;
+    querySnapShotList.forEach((QueryDocumentSnapshot<Map<String, dynamic>> element) {
+      print(element.data());
+    });
+
+    // return snapshot.data();
+  }
+
+  Future<String> create_club(
     String name,
     String leader,
     //초기에 지정 안되는 값
@@ -23,7 +59,7 @@ class ClubService extends ChangeNotifier {
     String todaygoal,
   ) async {
     // post 작성하기
-    await ClubCollection.add({
+    DocumentReference<Map<String, dynamic>> ref = await ClubCollection.add({
       'name': name,
       'leader': leader,
       //초기에 저장 안되는 값
@@ -33,6 +69,7 @@ class ClubService extends ChangeNotifier {
       'today_date': todaygoal,
     });
     notifyListeners(); // 화면 갱신
+    return ref.id;
   }
 
   void createmembers(
