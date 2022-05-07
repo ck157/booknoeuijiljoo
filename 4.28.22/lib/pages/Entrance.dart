d04d2008df263bdbdfc55cd626e17334d3773e6e
SONTAEGUK <101920873+SONTAEGUK@users.noreply.github.com>
2022-05-07 17:07:31 +0900

    Update Entrance.dart

--- 4.28.22/lib/pages/Entrance.dart
+++ 4.28.22/lib/pages/Entrance.dart
@@ -1,11 +1,11 @@
-import 'package:booknoejilju/pages/Lobby_members.dart';
-import 'package:booknoejilju/pages/create_club.dart';
-import 'package:booknoejilju/services/auth_service.dart';
-import 'package:booknoejilju/services/bookclub_service.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import 'Lobby.dart';
+import 'auth_service.dart';
+import 'bookclub_service.dart';
+import 'create_club.dart';
+import 'lobby_members.dart';
 
 void main() {
   runApp(EntrancePage());
@@ -30,7 +30,7 @@ class EntrancePage extends StatelessWidget {
                   child: Column(
                     children: [
                       Container(
-                        height: 30.0,
+                        height: 20.0,
                         width: 500.0,
                         color: Colors.black,
                       ),
@@ -39,23 +39,37 @@ class EntrancePage extends StatelessWidget {
                         style: TextStyle(fontSize: 20, color: Colors.white),
                       ),
                       Container(
-                        height: 40.0,
+                        height: 100.0,
                         width: 500.0,
                         color: Colors.black,
                       ),
                       Container(
-                        child: Image.asset('lib/images/Splash_Character.png'),
-                        height: 180.0,
-                        width: 400.0,
+                        child: Image.asset('lib/images/egg.png'),
+                        height: 150.0,
+                        width: 300.0,
                       ),
                       Container(
                         height: 20.0,
                         width: 500.0,
                         color: Colors.black,
                       ),
-                      Text(
-                        'bookiebookie33',
-                        style: TextStyle(fontSize: 20, color: Colors.blue),
+                      Container(
+                        width: 200,
+                        child: TextFormField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.red),
+                            ),
+                            suffixIcon: Icon(
+                              CupertinoIcons.pen,
+                              color: Colors.white,
+                            ),
+                            hintText: '닉네임을 입력해주세요',
+                            hintStyle: TextStyle(color: Colors.grey[600]),
+                          ),
+                        ),
                       ),
                       Container(
                         height: 50.0,
@@ -74,7 +88,7 @@ class EntrancePage extends StatelessWidget {
                         },
                         style: TextButton.styleFrom(
                           backgroundColor: Colors.red,
-                          minimumSize: Size(380, 50),
+                          minimumSize: Size(360, 50),
                         ),
                         label: Text(
                           '내 북클럽 입장하기',
@@ -86,13 +100,13 @@ class EntrancePage extends StatelessWidget {
                         icon: Icon(Icons.add, size: 18, color: Colors.white),
                       ),
                       Container(
-                        height: 60.0,
+                        height: 40.0,
                         width: 500.0,
                         color: Colors.black,
                       ),
                       Text(
                         '''다른 사람의 초대번호를 입력하고
-                  북클럽 방으로 입장해보세요.''',
+      북클럽 방으로 입장해보세요.''',
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: 15,
@@ -104,39 +118,33 @@ class EntrancePage extends StatelessWidget {
                         color: Colors.black,
                       ),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Container(
+                            height: 50.0,
                             width: 200.0,
+                            color: Colors.grey[850],
                             child: Expanded(
                               child: TextField(
                                 controller: _codecontroller,
                                 textAlign: TextAlign.center,
                                 style: TextStyle(color: Colors.white),
                                 decoration: InputDecoration(
-                                  labelText: "영문,숫자 9자리 조합",
+                                  labelText: "영문,숫자 20자리 조합",
                                   labelStyle: TextStyle(color: Colors.grey),
                                 ),
                               ),
                             ),
                           ),
-                          SizedBox(
-                            width: 10.0,
-                          ),
                           Container(
-                            height: 20.0,
+                            height: 50.0,
                             width: 2.0,
                             color: Colors.grey,
                           ),
-                          SizedBox(
-                            width: 10.0,
-                          ),
                           ElevatedButton.icon(
-                            onPressed: () async {
-                              List docIdList = await clubService.getClubList();
+                            onPressed: () {
+                              List docIdList = clubService.getClubList();
                               if (docIdList.contains(_codecontroller.text)) {
-                                clubService.updateuserdocId(
-                                    user.uid, _codecontroller.text);
-
                                 clubService.createmembers(
                                     user.uid, _codecontroller.text);
                                 Navigator.push(
@@ -152,7 +160,7 @@ class EntrancePage extends StatelessWidget {
                               }
                             },
                             style: TextButton.styleFrom(
-                              backgroundColor: Colors.grey,
+                              backgroundColor: Colors.grey[850],
                               minimumSize: Size(30, 50),
                             ),
                             label: Text(
